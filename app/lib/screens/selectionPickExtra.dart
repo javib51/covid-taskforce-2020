e4f8199c8900fdd5c754cc54f37b6ef38d5ddcdb
Ferra <ferranmontraveta@outlook.es>
2020-04-05 01:38:07 +0200

    added dynamic screen for extra pick up

--- app/lib/screens/selectionPickExtra.dart
+++ app/lib/screens/selectionPickExtra.dart
@@ -31,7 +31,7 @@ class _SelectionPickExtraPageState extends State<SelectionPickExtraPage> {
         margin: EdgeInsets.only(
             left: getSizeHeight(context, 7),
             right: getSizeHeight(context, 7),
-            top: getSizeHeight(context, 15),
+            top: getSizeHeight(context, 10),
             bottom: getSizeHeight(context, 7)),
         child: Center(
           child: ListView(children: <Widget>[
@@ -93,7 +93,111 @@ class _SelectionPickExtraPageState extends State<SelectionPickExtraPage> {
               ),
             ),
             SizedBox(
-              height: getSizeHeight(context, 5),
+              height: getSizeHeight(context, 2),
+            ),
+            SizedBox(
+              child:  Stack(
+                  children: <Widget>[
+                    Visibility(
+                      visible: !selected,
+                      child: Container(
+                          color: paleGreenTextBackground,
+                          padding: EdgeInsets.all(getSizeWidth(context, 3)),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Column(
+                                  children: [
+                                    Icon(
+                                      Icons.error_outline,
+                                      color: Colors.black,
+                                      size: 25.0,
+                                    ),
+                                  ],
+                                ),
+                                Column(
+                                  children: <Widget>[
+                                    SizedBox(width: getSizeWidth(context, 2),)
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Container(
+                                      width: getSizeWidth(context, 50),
+                                      child: Text("Please consider helping drop off other people orders. We will match you to people who live near you or live on your way back home!",
+                                          textAlign: TextAlign.justify,
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: getSizeHeight(context, 1.5),
+                                            color: blackText,
+                                          )),
+                                    )
+                                  ],
+                                ),
+                              ]
+                          )
+                      ),
+                    ),
+                    Visibility(
+                      visible: selected,
+                      child: Container(
+                          padding: EdgeInsets.all(getSizeWidth(context, 3)),
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Visibility(
+                                  visible: selectedButton == 1,
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        width: getSizeWidth(context, 60),
+                                        child: Text("Awesome! We’ll send you info on who’s order you’ll help with soon",
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.w500,
+                                              fontSize: getSizeHeight(context, 3.3),
+                                              color: purpleButtonActive,
+                                            )),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    SizedBox(height: getSizeHeight(context, 4),)
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Container(
+                                      height: getSizeHeight(context, 8),
+                                      padding: EdgeInsets.only(left: getSizeWidth(context, 10), right: getSizeWidth(context, 10)),
+                                      child: FlatButton(
+                                        color: purpleButtonActive,
+                                        disabledColor: purpleButtonNonActive,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: new BorderRadius.circular(12.0),
+                                        ),
+                                        child: Text("Confirm order",
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.w500,
+                                              fontSize: getSizeHeight(context, 2),
+                                              color: turquoiseLightButton,
+                                            )),
+                                        onPressed: () {
+                                          //                  Navigator.pushNamed(context, AppRoutes.map);
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ]
+                          )
+                      ),
+                    ),
+                  ],
+                )
             ),
           ]),
         ),
