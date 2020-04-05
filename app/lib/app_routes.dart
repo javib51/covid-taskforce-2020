8b4fc2325e4848fc1dd8f6236ad4d6b266c87eab
Ferra <ferranmontraveta@outlook.es>
2020-04-05 02:39:40 +0200

    Linking screens

--- app/lib/app_routes.dart
+++ app/lib/app_routes.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:covid/screens/pickup_selection.dart';
 import 'package:covid/screens/timeframe_selection_pickup.dart';
 import 'package:covid/screens/selectionPickExtra.dart';
+import 'package:covid/screens/order_confirmed.dart';
 
 class AppRoutes {
 
@@ -20,6 +21,8 @@ class AppRoutes {
   static const pickupselection = '/pickupselection';
   static const timeframeSelectionPickup = '/timeframePickup';
   static const selectionPickExtra = '/selectionPickExtra';
+  static const orderConfirmed = '/orderConfirmed';
+  static const pickupSelection = '/pickupSelection';
 
   static Map<String, WidgetBuilder> getRoutes() {
     return <String, WidgetBuilder>{
@@ -31,6 +34,8 @@ class AppRoutes {
       AppRoutes.pickupselection: (context) => PickupSelection(),
       AppRoutes.timeframeSelectionPickup: (context) => TimeframeSelectionPickPage(),
       AppRoutes.selectionPickExtra: (context) => SelectionPickExtraPage(),
+      AppRoutes.orderConfirmed: (context) => OrderConfirmed(),
+      AppRoutes.pickupSelection: (context) => PickupSelection(),
     };
   }
 }
